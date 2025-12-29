320c475cc36060ccf905d4bdc89aa13c498b2eb4
Erick <erickchhuang@gmail.com>
2025-12-29 15:49:06 -0800

    adding updated HashTable

--- HashTable/firstUniqChar.py
+++ HashTable/firstUniqChar.py
@@ -12,10 +12,15 @@ class Solution:
             else: 
                 h[key] = 1
         # index out the key that has a value of greater than 1
-        key = [key for key, value in h.items() if value == 1]
+        # key = [key for key, value in h.items() if value == 1]
+        firstKey = 0
+        for key, val in h.items():
+            if val == 1:
+                firstKey = key
+                break
         # loop through the string to return the index position of the string
         for index in range(len(s)):
-            if s[index] == key:
+            if s[index] == firstKey:
                 return index
         # else return -1
         return -1
